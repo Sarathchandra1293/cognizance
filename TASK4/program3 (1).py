d77b429ad4303fe905bb33f7bcb294b58f7fb448
sarathchandra edubelli <99632999+Sarathchandra1293@users.noreply.github.com>
2022-02-23 11:50:14 +0530

    Delete program3 (1).py

--- TASK4/program3 (1).py
+++
@@ -1,9 +0,0 @@
-store = [ [1,'abc',90],
-         [2,'bcd',95],
-         [3,'def',85] ]
-
-print("{:<9} {:<9} {:<9}".format('Roll No','Name','Marks'))
-for x in store:
-    for y in x:
-        print(y,end=" "*9)
-    print()
